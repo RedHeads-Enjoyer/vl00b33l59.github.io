4a1302cda41a762b12e46db48a4a95b0728c2495
Weestle <dsc.cxc@mail.ru>
2023-01-13 11:42:11 +0300

    123

---
+++ script.js
@@ -0,0 +1,17 @@
+let a, b, c
+setInterval(function() {
+    a = Math.random() * 255
+    b = Math.random() * 255
+    c = Math.random() * 255
+    document.body.style.background = "rgb(" + a + ", " + b + ", " + c + ")";
+    document.querySelector('#ss').style.color = "rgb(" + a + ", " + b + ", " + c + ")";
+},100)
+
+dance = document.getElementsByTagName("img")
+setInterval(function() {
+    for (let i = 0; i < dance.length; i++) {
+        dance[i].style.top = Math.random() * 80 - 10 + "vh"
+        dance[i].style.left = Math.random() * 80 + "vw"
+        dance[i].style.transform = "rotate(" + Math.random() * 360 + "deg)"
+    }
+}, 500)
