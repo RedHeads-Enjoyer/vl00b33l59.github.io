b4a921e37cc1c1edeafb75c1968edd6f8d7d9f8d
Weestle <dsc.cxc@mail.ru>
2023-01-13 14:39:05 +0300

    123

--- script.js
+++ script.js
@@ -8,13 +8,22 @@ setInterval(function() {
 },100)
 
 dance = document.getElementsByTagName("img")
+// setInterval(function() {
+//     for (let i = 2; i < dance.length - 2; i++) {
+//         dance[i].style.top = Math.random() * 80 - 10 + "vh"
+//         dance[i].style.left = Math.random() * 80 + "vw"
+//         dance[i].style.transform = "rotate(" + Math.random() * 360 + "deg)"
+//     }
+// }, 500)
+
+let i = 2;
 setInterval(function() {
-    for (let i = 2; i < dance.length - 1; i++) {
-        dance[i].style.top = Math.random() * 80 - 10 + "vh"
-        dance[i].style.left = Math.random() * 80 + "vw"
-        dance[i].style.transform = "rotate(" + Math.random() * 360 + "deg)"
-    }
-}, 500)
+    if (i > 7) i = 2;
+    dance[i].style.top = Math.random() * 80 - 10 + "vh"
+    dance[i].style.left = Math.random() * 80 + "vw"
+    dance[i].style.transform = "rotate(" + Math.random() * 360 + "deg)"
+    i += 1
+}, 60)
 
 
 function nazvanie() {
